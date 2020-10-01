039299c5bc4e44d5bf1187c621ee47fdc99395d1
Koduri Rajender <koduri.rajender@lsinextgen.com>
2020-10-01 14:12:15 +0530

    Add project files.

---
+++ Controllers/RoleController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FMRRoles.Data;
+using FMRRoles.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FMRRoles.Controllers
+{
+    [Route("api/[Controller]")]
+    public class RoleController : Controller
+    {
+        private Context _context;
+        public RoleController(Context context)
+        {
+            _context = context;
+        }
+        [HttpGet]
+        public List<Role> Get()
+        {
+            return _context.Role.ToList();
+        }
+       
+    }
+}
---
+++ Data/Context.cs
@@ -0,0 +1,22 @@
+﻿using FMRRoles.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web.Providers.Entities;
+
+namespace FMRRoles.Data
+{
+    public class Context:DbContext
+    {
+        public Context(DbContextOptions<Context>options):base(options)
+        {
+             
+        }
+        public DbSet<Role> Role { get; set; }
+     
+    
+    
+    }
+}
